0e279e226d7a54cdf8f8e2d19c0b8c35f14e6828
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-04-01 02:03:58 +0800

    updated database config

--- config/database.js
+++ config/database.js
@@ -2,13 +2,12 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      connector: 'mongoose',
+      settings: { 
+        url: env('DATABASE_URL'),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
